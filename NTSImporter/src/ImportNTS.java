39faa652e70d13efee8405766f7a63cde888351a
Bernhard Snizek <bs@metascapes.org>
2013-01-11 08:32:12 +0100

    yesh

---
+++ NTSImporter/src/ImportNTS.java
@@ -0,0 +1,17 @@
+
+public class ImportNTS {
+	
+	
+	public void loadCodes() {
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
